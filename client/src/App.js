fe2c4f67ce575e6b5b3d682b306a61e97f0af21f
이요한 <jcrinkey89120@gmail.com>
2020-12-23 13:42:35 +0900

    Add Customer Delete Module

--- client/src/App.js
+++ client/src/App.js
@@ -94,6 +94,7 @@ class App extends Component {
                 <TableCell>생년원일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -101,6 +102,7 @@ class App extends Component {
                 this.state.customers.map((c) => {
                   return (
                     <Customer
+                      stateRefresh={this.stateRefresh}
                       key={c.id}
                       id={c.id}
                       image={c.image}
